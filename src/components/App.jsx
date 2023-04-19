0322b7edcdc1d19a2e2dc61df882fe77841d66f9
sarah-die <saridi@alice.de>
2023-04-19 12:53:06 +0200

    Bis einschließlich Kapitel 4 (lifecycle (Hälfte))

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,16 @@ import React from "react";
 
 export class App extends React.Component {
   state = {
-    counterValue: 3,
+    // Countdown startvalue
+    counterValue: 10,
   };
 
+  // Methode wird aufgerufen, wenn die Komponente gemountet wurde
   componentDidMount() {
     setInterval(() => {
+      // setState = aktuellen State mit einem neuen State überschreiben
+      // hierbei kann setState eine Funktion entgegennehmen
+      // diese Funktion erhält den alten State als Parameter und gibt den neuen State zurück
       this.setState((state) => {
         if (state.counterValue > 0) {
           return { counterValue: state.counterValue - 1 };
@@ -14,7 +19,13 @@ export class App extends React.Component {
           return { counterValue: 0 };
         }
       });
+      // Alternativ:
+      // this.setState({
+      //   counterValue: Math.max(0, this.state.counterValue -1),
+      // });
+      //
     }, 1000);
+    // timeout = 1000 millisekunden = 1 sekunde
   }
 
   render() {
