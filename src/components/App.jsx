731567c3f4d4c5a9d6d5acd50a47a6df08bff510
sarah-die <saridi@alice.de>
2023-04-19 16:19:25 +0200

    Kapitel 5 einschließlich 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from "react";
 
 export function App() {
+    // neuer State
   const [countdown, setCountdown] = useState(10);
+
+  // Alternativ zu componentDidMount
+  // erster Parameter: Funktion, die bei jeder Änderung der Komponente aufgerufen wird
   useEffect(() => {
     const ref = setTimeout(() => {
       if (countdown > 0) {
@@ -9,8 +13,19 @@ export function App() {
       }
     }, 1000);
 
+    // aufräumen
     return () => clearTimeout(ref);
+    // zweiter Parameter nimmt dep-Array entgegen
   }, [countdown]);
+
+  // kein dep-Array = auf jedes Update reagieren
+  // leeres Array beim Mounten:
+  //const ref = setIntervall(() => {
+  //    if (countdown > 0) {
+  //        setCountdown(countdown - 1);
+  //    }
+  //}, 1000);
+  // dep = in Abhängigkeit von Änderungen derer
 
   return (
     <div>
