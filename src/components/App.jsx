5b85cd02f5849620b441797340ed8f7b6c0f77d5
sarah-die <saridi@alice.de>
2023-04-19 15:26:17 +0200

    Bis einschließlich Kapitel 4 abgeschlossen

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,12 @@ import React from "react";
 
 export class App extends React.Component {
   state = {
-    counterValue: 2,
+    counterValue: 10,
   };
 
   componentDidMount() {
     console.log("neues interval");
+    // this erzeugt neue Referenz
     this.myInterval = setInterval(() => {
       this.setState((state) => {
         if (state.counterValue > 0) {
@@ -18,19 +19,23 @@ export class App extends React.Component {
     }, 1000);
   }
 
+  // sicherstellen, dass Komponente aufgeräumt wird
   componentWillUnmount() {
     clearInterval(this.myInterval);
   }
 
+  // beobachten, wie sich die Werte verändern
+  // oder Differenzen zum vorherigen Wert darstellen
   componentDidUpdate(prevProps, prevState) {
     console.log(prevProps, prevState);
   }
 
+  // Intervall läuft unendlich lange, wenn die 0 erreich wird
+  // Lösung:
   shouldComponentUpdate() {
     if (this.state.counterValue === 0) {
       return false;
     }
-
     return true;
   }
 
