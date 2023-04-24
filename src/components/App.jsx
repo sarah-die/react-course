f641ba9d70deef137ef05d8fec16ac7c08c6cca7
sarah-die <saridi@alice.de>
2023-04-24 10:10:17 +0200

    Kapitel 7.1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,13 +5,18 @@ export function App() {
   const [inputValue, setInputValue] = useState("");
 
   const buttonClicked = () => {
+      // alert("Button was clicked!")
     setHeadline(inputValue);
   };
 
+  // Input vom Textfeld auslesen
   const inputChanged = (event) => {
+      // event.target = Referenz zu Input-Element
+      // mit value auf den Wert zugreifen
     setInputValue(event.target.value);
   };
 
+    // onInput = Input-Event
   return (
     <div>
       <h1>{headline}</h1>
