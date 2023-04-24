1194916dc931574d779660e9c7fe84bb2a819455
sarah-die <saridi@alice.de>
2023-04-24 10:38:34 +0200

    Kapitel 7.2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,15 @@ import { StatusChanger } from "./StatusChanger";
 
 export function App() {
   const [status, setStatus] = useState("Verfügbar ✅");
+
+  // 3)
+  // onStatusChange im StatusChanger bekommt Status übergeben; dieser (newStatus) kann hier ausgelesen werden
   const onStatusChange = (newStatus) => setStatus(newStatus);
 
+  // 1)
+  // da StatusChanger eine CustomComponent ist, kann onClick={buttonClicked} nicht funktionieren
+  // jede Property, die an Custom Components übergeben wird, wird in die Props geschrieben
+  // übergabe von beliebig benamten Funktionen ist möglich (hier onStatusChange)
   return (
     <div>
       <h1>Status: {status}</h1>
--- src/components/StatusChanger.jsx
+++ src/components/StatusChanger.jsx
@@ -1,5 +1,9 @@
 export function StatusChanger(props) {
   const newStatus = props.statusText;
+
+  // 2)
+  // onStatusChange wurde in App.js definiert und übergeben
+  // hier wird es über props abgerufen
   const buttonClicked = () => props.onStatusChange(newStatus);
 
   return (
