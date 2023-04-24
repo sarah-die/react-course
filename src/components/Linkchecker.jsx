c469dbf8a08281f164a1f325cf8d2fc036ac4b0b
sarah-die <saridi@alice.de>
2023-04-24 11:08:03 +0200

    Kapitel 7.3

--- src/components/Linkchecker.jsx
+++ src/components/Linkchecker.jsx
@@ -1,12 +1,20 @@
 import { useEffect, useState } from "react";
 
+// Komponente zeigt den Link an, über den man hovert
 export function Linkchecker() {
   const [link, setLink] = useState("");
 
+  // globaler Event-Listener für alle mouseover-Events
+  // leeres DepArray, damit nur ein EventListener registriert wird
   useEffect(() => {
+
+    // diese Funktion soll bei mouseover aufgerufen werden
     const mouseover = (event) => {
+      // betroffenes Element/ Referenz darauf
       const domElem = event.target;
+      // AnchorTag = Link
       if (domElem.nodeName === "A") {
+        // event.target.href
         setLink(domElem.href);
       } else {
         setLink("");
@@ -15,6 +23,7 @@ export function Linkchecker() {
 
     window.addEventListener("mouseover", mouseover);
 
+    // cleanUp beim unmounten der component mit der selben Funktionsreferenz
     return () => {
       window.removeEventListener("mouseover", mouseover);
     };
