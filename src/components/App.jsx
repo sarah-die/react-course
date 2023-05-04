fc41f73a4e5c67f808992ee7c404018e60f5e15e
sarah-die <saridi@alice.de>
2023-05-04 10:17:34 +0200

    Kapitel 11.1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,18 +2,25 @@ import React, { useRef, useState } from "react";
 
 export function App() {
   const [formData, setFormData] = useState({});
+  // Referenzcontainer (alternativ zum Formular) -> diesen hier als Ref-Attribut an Input-Feld weitergeben
   const textFieldRef = useRef();
 
   const formSubmitted = (submitEvent) => {
+    // neuladen (standard Ausführung des Events) unterdrücken mit prevent.Default()
     submitEvent.preventDefault();
     const form = submitEvent.target;
+    // alle formControls
     const formControls = form.elements;
+    // const fullnameInput = formControls.fullname (verlinkt via htmlFor)
+    // Alternative zum Formular: Referenzen zu einzelnen Input-Feldern erzeugen
     const fullnameInput = textFieldRef.current;
     const birthdateInput = formControls.birthdate;
 
+    // Werte aus Feldern auslesen
     const fullname = fullnameInput.value;
     const birthdate = birthdateInput.value;
 
+    // Werte in den State setzen
     setFormData({ fullname, birthdate });
   };
 
@@ -24,6 +31,7 @@ export function App() {
       </header>
 
       <main>
+        // onSubmit = eventListener
         <form onSubmit={formSubmitted}>
           <fieldset>
             <legend>Persönliche Daten</legend>
@@ -32,6 +40,7 @@ export function App() {
               <label htmlFor="fullname">Name:</label>
               <br />
               <input
+                // Alternativ zu Formularen via Referenzen
                 ref={textFieldRef}
                 type="text"
                 id="fullname"
@@ -61,6 +70,7 @@ export function App() {
         </form>
 
         <h2>Abgesendete Daten:</h2>
+        // Daten ausgeben:
         {JSON.stringify(formData)}
       </main>
     </>
